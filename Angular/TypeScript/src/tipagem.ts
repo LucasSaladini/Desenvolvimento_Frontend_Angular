3f499b0fd4c670db91b4770cdad8c99632b07cd7
Lucas Saladini <lucassaladini@hotmail.com>
2023-11-30 19:59:31 -0300

    Working with typing

---
+++ Angular/TypeScript/src/tipagem.ts
@@ -0,0 +1,15 @@
+//Tipos primitivos
+let ligado:boolean = false
+let nome:string = 'Lucas'
+let idade:number = 30
+let altura:number = 1.83
+
+//Tipos especiais
+let nulo:null = null
+let indefinido:undefined = undefined
+
+//Tipos abrangentes
+let retorno:void = void
+function executarQuery():void {}
+
+let retornoView:any = "Aceita qualquer coisa (retorno não previsível)"
