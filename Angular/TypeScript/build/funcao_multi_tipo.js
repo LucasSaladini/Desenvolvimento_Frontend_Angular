989315ab887ecb7b4914b104871143238c6afe96
Lucas Saladini <lucassaladini@hotmail.com>
2023-12-01 18:30:35 -0300

    Working with multi type functions

---
+++ Angular/TypeScript/build/funcao_multi_tipo.js
@@ -0,0 +1,6 @@
+"use strict";
+function callToPhone(phone) {
+    return phone;
+}
+console.log(callToPhone(22424442));
+console.log(callToPhone('22424442'));
---
+++ Angular/TypeScript/src/funcao_multi_tipo.ts
@@ -0,0 +1,6 @@
+function callToPhone(phone: number | string): number | string {
+    return phone
+}
+
+console.log(callToPhone(22424442))
+console.log(callToPhone('22424442'))
