4ecc562f8c95b46dd799b9e07c4eb99d37b37332
Lucas Saladini <lucassaladini@hotmail.com>
2023-12-02 10:44:05 -0300

    Using interface

--- Angular/TypeScript/build/interface.js
+++ Angular/TypeScript/build/interface.js
@@ -5,7 +5,21 @@ const bot = {
 };
 const bot2 = {
     id: 2,
-    name: 'megaman'
+    name: 'megaman',
+    sayHello: function () {
+        throw new Error("Function not implemented.");
+    }
 };
 console.log(bot);
 console.log(bot2);
+class Pessoa {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return `Hello I'm ${this.name}`;
+    }
+}
+const p = new Pessoa(1, "gutsman");
+console.log(p.sayHello());
--- Angular/TypeScript/src/interface.ts
+++ Angular/TypeScript/src/interface.ts
@@ -15,12 +15,38 @@ const bot: robot = {
 interface robot2 {
     readonly id: number | string
     name: string
+    sayHello(): string
 }
 
 const bot2: robot2 = {
     id: 2,
-    name: 'megaman'
+    name: 'megaman',
+    sayHello: function (): string {
+        throw new Error("Function not implemented.")
+    }
 }
 
 console.log(bot)
 console.log(bot2)
+
+
+
+
+
+class Pessoa implements robot2 {
+    id: string | number
+    name: string
+    
+    constructor(id: string | number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    sayHello(): string {
+        return `Hello I'm ${this.name}`
+    }
+}
+
+const p = new Pessoa(1, "gutsman")
+
+console.log(p.sayHello())
