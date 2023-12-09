5ee4b0621c0a915995cc1f05f856409e1e56dd24
Lucas Saladini <lucassaladini@hotmail.com>
2023-12-09 11:17:13 -0300

    Styled and fetching data dynamically

--- Angular_Framework/services/src/app/components/card/card.component.ts
+++ Angular_Framework/services/src/app/components/card/card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  name: string = "Bulbassaur"
+  attributesTypes: string[] = ['Fire', 'Rock']
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
